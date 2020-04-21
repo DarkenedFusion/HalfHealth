b08c897f5ffef05f8b6eba31706f2c9fc17498d9
BuildTools <unconfigured@null.spigotmc.org>
2020-04-21 15:06:08 -0400

    - Created possible working command

--- src/main/java/io/github/darkenedfusion/HalfHealth/Commands/HalfHealth.java
+++ src/main/java/io/github/darkenedfusion/HalfHealth/Commands/HalfHealth.java
@@ -1,5 +1,6 @@
 package io.github.darkenedfusion.HalfHealth.Commands;
 
+import org.bukkit.ChatColor;
 import org.bukkit.attribute.Attribute;
 import org.bukkit.attribute.AttributeModifier;
 import org.bukkit.command.Command;
@@ -13,20 +14,22 @@ public class HalfHealth implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         Player player = (Player) sender;
-        player.getAttribute(Attribute.GENERIC_MAX_HEALTH);
+
+        player.getAttribute(Attribute.GENERIC_MAX_HEALTH).setBaseValue(5);
 
         return true;
     }
 
 
 
-    private void setMaxHealth(Player player){
-        int MaxHealth = 5;
-
-
-
+    private void playHealthTitle(Player player) {
+        String title = ChatColor.DARK_RED + "Max heal is now 5!";
+        String subTitle = null;
+        int duration = 50;
+        int fadeIn = 10;
+        int fadeOut = 5;
+        player.sendTitle(title, null, duration, fadeIn, fadeOut);
     }
-
 }
 
 
